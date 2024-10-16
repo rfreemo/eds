311bd2db4301a342ac64615f54b16433afb192b0
Rob Freeman <rfreeman@adobe.com>
2024-10-16 17:53:07 +0200

    move GTM / GA JS to delyed

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,11 +1,12 @@
-(function() {
-  var gtagScript = document.createElement('script');
+(() => {
+  const gtagScript = document.createElement('script');
   gtagScript.async = true;
   gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-59C6YZDTJY';
   document.head.appendChild(gtagScript);
 
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  // eslint-disable-next-line no-undef
+  const gtag = (...args) => dataLayer.push(args);
   gtag('js', new Date());
 
   gtag('config', 'G-59C6YZDTJY');
