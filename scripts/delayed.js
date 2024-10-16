0b49f5c226a3ae4b22742845e8ffbc0afa13c500
Rob <71897226+rfreemo@users.noreply.github.com>
2024-10-16 15:41:44 +0200

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,9 +1,12 @@
-// add delayed functionality here
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-59C6YZDTJY"></script>
-<script>
+(function() {
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-59C6YZDTJY';
+  document.head.appendChild(gtagScript);
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-59C6YZDTJY');
-</script>
+})();
