9b8244404be1cb8204d1a9e01f6aefebc5bca443
Rob <71897226+rfreemo@users.noreply.github.com>
2024-10-20 21:26:29 +0200

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,15 +1,1 @@
-(() => {
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push({
-    'gtm.start': new Date().getTime(),
-    event: 'gtm.js',
-  });
 
-  const gtmScript = document.createElement('script');
-  gtmScript.async = true;
-  const gtmId = 'GTM-K9KKVKV7';
-  gtmScript.src = `https://www.googletagmanager.com/gtm.js?id=${gtmId}`;
-
-  const firstScript = document.getElementsByTagName('script')[0];
-  firstScript.parentNode.insertBefore(gtmScript, firstScript);
-})();
